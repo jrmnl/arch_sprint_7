fd4aa9cdcf92d634ba5b8859d297857e00654ffa
Тимаков Владислав Андреевич <vtimakov@alfabank.ru>
2024-12-02 00:51:59 +0300

    упрощение

--- Exc4/bind-roles.bash
+++ Exc4/bind-roles.bash
@@ -1,3 +1,3 @@
-kubectl apply -f ./role-bindings/cluster-edit-resources.yaml
-kubectl apply -f ./role-bindings/cluster-watch-resources.yaml
+kubectl apply -f ./role-bindings/cluster-edit.yaml
+kubectl apply -f ./role-bindings/cluster-view.yaml
 kubectl apply -f ./role-bindings/cluster-watch-secrets.yaml
--- Exc4/create-roles.bash
+++ Exc4/create-roles.bash
@@ -1,3 +1,1 @@
 kubectl apply -f ./roles/secrets-watcher.yaml
-kubectl apply -f ./roles/resources-watcher.yaml
-kubectl apply -f ./roles/resources-editor.yaml
